34bdaa90944be4507d8f162a8050c81c80b308bc
neapovea <neapovea@gmail.com>
2026-06-03 10:35:08 +0200

    Add files via upload
    Limpieza synchronized

---
+++ Log.java
@@ -0,0 +1,167 @@
+/*
+* Copyright (c) Joan-Manuel Marques 2013. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This file is part of the practical assignment of Distributed Systems course.
+*
+* This code is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this code.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package recipes_service.tsae.data_structures;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
+
+import recipes_service.data.Operation;
+//LSim logging system imports sgeag@2017
+//import lsim.coordinator.LSimCoordinator;
+import edu.uoc.dpcs.lsim.logger.LoggerManager.Level;
+import lsim.library.api.LSimLogger;
+
+/**
+ * @author Joan-Manuel Marques, Daniel Lázaro Iglesias
+ * December 2012
+ *
+ */
+public class Log implements Serializable{
+	// Only for the zip file with the correct solution of phase1.Needed for the logging system for the phase1. sgeag_2018p 
+//	private transient LSimCoordinator lsim = LSimFactory.getCoordinatorInstance();
+	// Needed for the logging system sgeag@2017
+//	private transient LSimWorker lsim = LSimFactory.getWorkerInstance();
+
+	private static final long serialVersionUID = -4864990265268259700L;
+	/**
+	 * This class implements a log, that stores the operations
+	 * received  by a client.
+	 * They are stored in a ConcurrentHashMap (a hash table),
+	 * that stores a list of operations for each member of 
+	 * the group.
+	 */
+	private final ConcurrentHashMap<String, CopyOnWriteArrayList<Operation>> log = new ConcurrentHashMap<>();
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+
+	public Log(List<String> participants){
+		// create an empty log
+		for (String participant : participants) {
+			log.put(participant, new CopyOnWriteArrayList<>());
+		}
+	}
+
+	/**
+	 * inserts an operation into the log. Operations are 
+	 * inserted in order. If the last operation for 
+	 * the user is not the previous operation than the one 
+	 * being inserted, the insertion will fail.
+	 * 
+	 * @param op
+	 * @return true if op is inserted, false otherwise.
+	 */
+
+
+	public synchronized boolean add(Operation op){
+		// Recuperar el ID del host
+		String hostId = op.getTimestamp().getHostid();
+		// Recuperar el log del host
+		// Obtener o inicializar la lista de operaciones para este host
+		CopyOnWriteArrayList<Operation> operationsList = log.computeIfAbsent(hostId, key -> new CopyOnWriteArrayList<>());
+
+		// Comparar si esta vacia o tiempo actual con la última entrada son iguales
+		if (operationsList.isEmpty() || operationsList.get(operationsList.size() - 1).getTimestamp().compare(op.getTimestamp()) < 0) {
+			operationsList.add(op);
+			return true;
+		}
+		return false;
+	}
+
+
+
+	/**
+	 * @param sum The sum of timestamps to compare against.
+	 * @return A list of operations that are newer than the given sum of timestamps.
+	 */
+	public synchronized List<Operation> listNewer(TimestampVector partnerSummary) {
+		List<Operation> newOperations = new ArrayList<>();
+		// Iterar sobre las entradas del log (operaciones por host)
+		for (Map.Entry<String, CopyOnWriteArrayList<Operation>> entry : log.entrySet()) {
+			String hostId = entry.getKey();
+			CopyOnWriteArrayList<Operation> hostOperationsList = entry.getValue();
+
+			// saltar operaciones vacias
+			if (hostOperationsList.isEmpty())
+				continue;
+
+			Timestamp lastSeenByPartner = partnerSummary.getLast(hostId);
+			// Filtrar y añadir solo las operaciones que el compañero no ha visto
+			for (Operation op : hostOperationsList) {
+				if (op.getTimestamp().compare(lastSeenByPartner) > 0) {
+					newOperations.add(op);
+				}
+			}
+		}
+
+		return newOperations;
+	}
+	
+	/**
+	 * Removes from the log the operations that have
+	 * been acknowledged by all the members
+	 * of the group, according to the provided
+	 * ackSummary. 
+	 * @param ack: ackSummary.
+	 */
+	public void purgeLog(TimestampMatrix ack){
+		System.err.println("Error: purgeLog method (Log) not yet implemented");
+	}
+
+	/**
+	 * equals
+	 */
+	@Override
+	public synchronized boolean equals(Object obj) {
+		// Verificar de identidad y nulidad básica
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+
+		Log other = (Log) obj;
+		// Comparar el mapa log de la instancia actual con el de la other
+		return this.log.equals(other.log);
+	}
+
+
+
+	/**
+	 * toString
+	 */
+	@Override
+	public synchronized String toString() {
+			String name="";
+			for(Enumeration<CopyOnWriteArrayList<Operation>> en = log.elements();
+				en.hasMoreElements(); ){
+				List<Operation> sublog=en.nextElement();
+				for(ListIterator<Operation> en2=sublog.listIterator(); en2.hasNext();){
+					name+=en2.next().toString()+"\n";
+				}
+			}
+
+			return name;
+	}
+
+
+}
---
+++ ServerData.java
@@ -0,0 +1,293 @@
+/*
+* Copyright (c) Joan-Manuel Marques 2013. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This file is part of the practical assignment of Distributed Systems course.
+*
+* This code is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this code.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package recipes_service;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import edu.uoc.dpcs.lsim.logger.LoggerManager.Level;
+import lsim.library.api.LSimLogger;
+import recipes_service.activity_simulation.SimulationData;
+import recipes_service.communication.Host;
+import recipes_service.communication.Hosts;
+import recipes_service.data.AddOperation;
+import recipes_service.data.Operation;
+import recipes_service.data.Recipe;
+import recipes_service.data.Recipes;
+import recipes_service.data.RemoveOperation;
+import recipes_service.tsae.data_structures.Log;
+import recipes_service.tsae.data_structures.Timestamp;
+import recipes_service.tsae.data_structures.TimestampMatrix;
+import recipes_service.tsae.data_structures.TimestampVector;
+import recipes_service.tsae.sessions.TSAESessionOriginatorSide;
+/**
+ * @author Joan-Manuel Marques
+ * December 2012
+ *
+ */
+public class ServerData {
+	
+	// server id
+	private String id;
+	
+	// sequence number of the last recipe timestamped by this server
+	private AtomicLong seqnum = new AtomicLong(Timestamp.NULL_TIMESTAMP_SEQ_NUMBER);
+
+
+	// timestamp lock
+	//private Object timestampLock = new Object();
+	
+	// TSAE data structures
+	private Log log = null;
+	private TimestampVector summary = null;
+	private TimestampMatrix ack = null;
+	
+	// recipes data structure
+	private Recipes recipes = new Recipes();
+
+	// number of TSAE sessions
+	int numSes = 1; // number of different partners that a server will contact for a TSAE session each time that TSAE timer (each sessionPeriod seconds) expires
+
+	// propDegree: (default value: 0) number of TSAE sessions done each time a new data is created
+	int propDegree = 0;
+	
+	// Participating nodes
+	private Hosts participants;
+
+	// TSAE timers
+	private long sessionDelay;
+	private long sessionPeriod = 10;
+
+	private Timer tsaeSessionTimer;
+	//
+	TSAESessionOriginatorSide tsae = null;
+
+	// TODO: esborrar aquesta estructura de dades
+	// tombstones: timestamp of removed operations
+	//List<Timestamp> tombstones = new Vector<Timestamp>();
+	private List<Timestamp> tombstones = new CopyOnWriteArrayList<>();
+
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+	// end: true when program should end; false otherwise
+	private boolean end;
+
+	public ServerData(){
+	}
+	
+	/**
+	 * Starts the execution
+	 * @param participantss
+	 */
+	public void startTSAE(Hosts participants){
+		this.participants = participants;
+		this.log = new Log(participants.getIds());
+		this.summary = new TimestampVector(participants.getIds());
+		this.ack = new TimestampMatrix(participants.getIds());
+		
+
+		//  Sets the Timer for TSAE sessions
+	    tsae = new TSAESessionOriginatorSide(this);
+		tsaeSessionTimer = new Timer();
+		tsaeSessionTimer.scheduleAtFixedRate(tsae, sessionDelay, sessionPeriod);
+	}
+
+	public void stopTSAEsessions(){
+		if (tsaeSessionTimer != null) {
+			tsaeSessionTimer.cancel();
+		}
+	}
+	
+	public boolean end(){
+		return this.end;
+	}
+	
+	public void setEnd(){
+		this.end = true;
+	}
+
+	// ******************************
+	// *** timestamps
+	// ******************************
+	private Timestamp nextTimestamp(){
+		// Iniciar secuencia
+//		if (seqnum.get() == Timestamp.NULL_TIMESTAMP_SEQ_NUMBER) {
+//			seqnum.set(-1);
+//		}
+		// Generar nuevo timestamp con el ID del host y el incremento de la secuencia
+		return  new Timestamp(id, seqnum.incrementAndGet());
+	}
+
+	// ******************************
+	// *** add and remove recipes
+	// ******************************
+	public synchronized void addRecipe(String recipeTitle, String recipe) {
+
+		if (recipeTitle == null || recipe == null) {
+			LSimLogger.log(Level.WARN, "Invalid recipe input: title or content is null.");
+			return;
+		}
+
+		Timestamp timestamp = nextTimestamp();
+		Recipe rcpe = new Recipe(recipeTitle, recipe, id, timestamp);
+		Operation op = new AddOperation(rcpe, timestamp);
+
+		// actualizar las estructuras de datos del servidor
+		log.add(op);                        	// Añadir al log de mensajes para propagación
+		summary.updateTimestamp(timestamp); 	// Actualizar timestamp local del host
+		recipes.add(rcpe);                 		// Añadir Recipe
+
+		LSimLogger.log(Level.TRACE, "Recipe '" + recipeTitle + "' added to local storage and log.");
+
+	}
+	
+	public synchronized void removeRecipe(String recipeTitle){
+		System.err.println("Error: removeRecipe method (serverData) not yet implemented");
+	}
+
+	private synchronized void purgeTombstones() {
+		if (ack == null){
+			return;
+		}
+		TimestampVector sum = ack.minTimestampVector();
+
+		List<Timestamp> newTombstones = new Vector<Timestamp>();
+		for(int i=0; i<tombstones.size(); i++){
+			if (tombstones.get(i).compare(sum.getLast(tombstones.get(i).getHostid()))>0){
+				newTombstones.add(tombstones.get(i));
+			}
+		}
+		tombstones = newTombstones;
+	}
+
+
+	// ****************************************************************************
+	// *** operations to get the TSAE data structures. Used to send to evaluation
+	// ****************************************************************************
+	public Log getLog() {
+		return log;
+	}
+	public TimestampVector getSummary() {
+		return summary;
+	}
+	public TimestampMatrix getAck() {
+		return ack;
+	}
+	public Recipes getRecipes(){
+		return recipes;
+	}
+
+	// ******************************
+	// *** getters and setters
+	// ******************************
+	public void setId(String id){
+		this.id = id;		
+	}
+	public String getId(){
+		return this.id;
+	}
+
+	public int getNumberSessions(){
+		return numSes;
+	}
+
+	public void setNumberSessions(int numSes){
+		this.numSes = numSes;
+	}
+
+	public int getPropagationDegree(){
+		return this.propDegree;
+	}
+
+	public void setPropagationDegree(int propDegree){
+		this.propDegree = propDegree;
+	}
+
+	public void setSessionDelay(long sessionDelay) {
+		this.sessionDelay = sessionDelay;
+	}
+	public void setSessionPeriod(long sessionPeriod) {
+		this.sessionPeriod = sessionPeriod;
+	}
+	public TSAESessionOriginatorSide getTSAESessionOriginatorSide(){
+		return this.tsae;
+	}
+	
+	// ******************************
+	// *** other
+	// ******************************
+	
+	public List<Host> getRandomPartners(int num){
+		return participants.getRandomPartners(num);
+	}
+	
+	/**
+	 * waits until the Server is ready to receive TSAE sessions from partner servers   
+	 */
+	public synchronized void waitServerConnected(){
+		while (!SimulationData.getInstance().isConnected()){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				//			e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 	Once the server is connected notifies to ServerPartnerSide that it is ready
+	 *  to receive TSAE sessions from partner servers  
+	 */ 
+	public synchronized void notifyServerConnected(){
+		notifyAll();
+	}
+
+
+    public synchronized void registerOperation(Operation op) {
+		// Procesar operación a agregar
+		if (op instanceof AddOperation addOp) {
+			Recipe recipeData = addOp.getRecipe();
+
+			// Crear nueva instancia de Recipe para asegurar la integridad local
+			Recipe newRecipe = new Recipe(
+					recipeData.getTitle(),
+					recipeData.getRecipe(),
+					recipeData.getAuthor(),
+					recipeData.getTimestamp()
+			);
+
+			recipes.add(newRecipe);
+			//log.add(op); // Registrar en el log para futura propagación
+
+		}
+		// Procesar operación de eliminar
+		else if (op instanceof RemoveOperation removeOp) {
+			recipes.remove(removeOp.getRecipeTitle());
+		}
+    }
+
+}
