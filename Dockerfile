fb221162aad9d369a6fb4df59ba455237761574f
neapovea <neapovea@gmail.com>
2026-06-05 11:56:02 +0200

    Pruebas y validaciones del contenedor para lanzar compilación y ejecución

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 # Imagen base ligera de Java 17 basada en entorno Ubuntu
 FROM eclipse-temurin:17-jdk-jammy
 
+# Actualizamos repositorios, instalamos Git y limpiamos la caché temporal
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends git && \
+    rm -rf /var/lib/apt/lists/*
+
 # Directorio de trabajo dentro del contenedor
 WORKDIR /app
 
--- iniciar.sh
+++ iniciar.sh
@@ -3,11 +3,36 @@
 echo "-*---------------------------------------Probando FASE." $OPCION_FASE
 # 1. Comprobar si se ha pasado al menos un parámetro
 if [ -z "$OPCION_FASE" ]; then
-    echo "Error: No se ha proporcionado ningún parámetro."
+    echo "Error: No se ha proporcionado parámetro OPCION_FASE."
     echo "Uso: $0 <2|3|4>"
     exit 1
 fi
 
+if [ -z "$CANTIDAD_NODOS" ]; then
+    echo "Error: No se ha proporcionado parámetro CANTIDAD_NODOS."
+    echo "Uso: $0 <6|15|20>"
+    exit 1
+fi
+
+## segun la fase hago checkuout sobre la rama en concreto
+echo "Opción $OPCION_FASE recibida. Lanzando git sobre fase $OPCION_FASE"
+case "$OPCION_FASE" in
+    2)
+        git checkout FASE2
+        ;;
+    3)
+        git checkout FASE3
+        ;;
+    4)
+        git checkout FASE4
+        ;;
+    *)
+        # Aquí nunca llega
+        echo "Error: Parámetro no válido ('$OPCION_FASE')."
+        echo "Los valores permitidos son únicamente 2, 3 o 4."
+        exit 1
+        ;;
+esac
 
 echo "-*---------------------------------------Compilar."
 # COMPILAR
@@ -18,41 +43,32 @@ javac --release 17 -d bin -cp "lib/*" @sources.txt
 
 # MOSTRAR RESULTAdos
 echo "-*---------------------------------------Comprobar compilación."
-date
 echo "-*---------------------------------------Probando FASE." $OPCION_FASE
 ls  -lh bin/recipes_service/ServerData.class bin/recipes_service/tsae/data_structures/* bin/recipes_service/tsae/sessions/*  scripts/Results*; cat scripts/Results 
 
 
 
-# 2. Guardar el primer parámetro en una variable
-CANTIDAD_NODOS=15
+
 
 
 cd scripts
 chmod +x  start.sh 
 
 # 3. Evaluar el parámetro y lanzar el comando correspondiente
+echo "Opción $OPCION_FASE recibida. Lanzando comando para fase $OPCION_FASE con  $CANTIDAD_NODOS  nodos"
+date
 case "$OPCION_FASE" in
     2)
-        echo "Opción 2 recibida. Lanzando comando para el entorno 2..."
-        # Sustituye la siguiente línea por tu comando real (ej: compilación, ejecución Java, etc.)
-        # javac -d bin src/Main.java && java -cp bin Main --config=2
         ./start.sh 20004 $CANTIDAD_NODOS --logResults --path ../results --nopurge --noremove > resultados.fase2.txt 2>&1
         ;;
     3)
-        echo "Opción 3 recibida. Lanzando comando para el entorno 3..."
-        # Sustituye la siguiente línea por tu comando real
-        # javac -d bin src/Main.java && java -cp bin Main --config=3
         ./start.sh 20004 $CANTIDAD_NODOS --logResults --path ../results --noremove > resultados.fase3.txt 2>&1
         ;;
     4)
-        echo "Opción 4 recibida. Lanzando comando para el entorno 4..."
-        # Sustituye la siguiente línea por tu comando real
-        # javac -d bin src/Main.java && java -cp bin Main --config=4
         ./start.sh 20004 $CANTIDAD_NODOS --logResults --path ../results  > resultados.fase4.txt 2>&1
         ;;
     *)
-        # Esta opción (*) actúa como "default" y captura cualquier valor no contemplado
+        # Aquí nunca llega
         echo "Error: Parámetro no válido ('$OPCION_FASE')."
         echo "Los valores permitidos son únicamente 2, 3 o 4."
         exit 1
@@ -62,7 +78,7 @@ esac
 cd ..
 
 echo "-*---------------------------------------Comprobar resultados."
-ls  -lh bin/recipes_service/ServerData.class bin/recipes_service/tsae/data_structures/* bin/recipes_service/tsae/sessions/*  scripts/Results*; cat scripts/Results 
 date
+ls  -lh bin/recipes_service/ServerData.class bin/recipes_service/tsae/data_structures/* bin/recipes_service/tsae/sessions/*  scripts/Results*; cat scripts/Results 
 echo "-*---------------------------------------Ejecución finalizada."
 
